ac53b5f321fa055ba5fed5e2869d2d8b9255ef86
Pedro Mendes <mendes@copasi.org>
2026-01-10 15:15:33 -0500

    removed spurious folder

--- scid/scid/doinst.sh
+++
@@ -1,6 +0,0 @@
-if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database usr/share/applications &> /dev/null
-fi
-
-( cd usr/bin ; rm -rf scid )
-( cd usr/bin ; ln -sf ../share/scid/scid scid )
